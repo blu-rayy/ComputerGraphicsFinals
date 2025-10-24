16619f33eb96cd17368d7ecd7d247a6947cc7237
ddavidd <kristiandavidbautista@gmail.com>
2025-10-24 14:07:27 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <GL/freeglut.h>
+// g++ src/main.cpp -Ilibs/freeglut/include -Llibs/freeglut/lib -lfreeglut -lopengl32 -lglu32 -o app.exe
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_TRIANGLES);
+        glColor3f(1, 0, 0); glVertex2f(-0.5f, -0.5f);
+        glColor3f(0, 1, 0); glVertex2f( 0.5f, -0.5f);
+        glColor3f(0, 0, 1); glVertex2f( 0.0f,  0.5f);
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutCreateWindow("FreeGLUT + VS Code");
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}
